2f64e674e4b4939433a7bb502573efe6ba7b024d
Sanket <s.dorle@splendornet.com>
2019-04-30 17:00:51 +0530

    Display list of contact using Kendo Grid.

--- src/client/App.js
+++ src/client/App.js
@@ -4,26 +4,56 @@ import {
   Row, Col,
   Table
 } from 'reactstrap';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import { filterBy } from '@progress/kendo-data-query';
 import axios from 'axios';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      contacts: [],
+      filters: [
+        { field: "organization_id" }
+      ]
     };
   }
 
   componentDidMount() {
-    
+    this.getAllContacts();
+  }
+
+  getAllContacts() {
+    axios.get(`/contacts/`)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          contacts: res.data.organizations
+        })
+      })
   }
 
   render() {
-
-    
-
     return (
       <div>
         <Row align="center" className="header">Contacts List</Row>
+        <Grid
+          style={{ height: '100%', fontSize: "18px" }}
+          data={filterBy(this.state.contacts, this.state.filter)}
+          filterable
+          filter={this.state.filter}
+          onFilterChange={(e) => {
+            this.setState({
+              filter: e.filter
+            });
+          }}
+        >
+          <Column field="organization_id" title="Organization Id" />
+          <Column field="name" title="Name" />
+          <Column field="contact_name" title="Contact Name" />
+          <Column field="email" title="Email" />
+          <Column field="plan_name" title="Plan Name" />
+        </Grid>
       </div>
     );
   }
