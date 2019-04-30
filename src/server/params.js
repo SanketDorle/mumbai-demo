8b9593276334ac3fce67bbe61c2d4d0f58cab916
Sanket <s.dorle@splendornet.com>
2019-04-30 16:16:08 +0530

    Create an nodejs api endpoint for get contacts using zoho api endpoint.

---
+++ src/server/params.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "organization_id":"649249007",
+    "zohooauthtoken":"db36e02a50b57e081efe533a8a0f834b"
+}
--- src/server/routes/api.js
+++ src/server/routes/api.js
@@ -1,12 +1,35 @@
 // Dependencies
 const express = require('express');
 const router = express.Router();
+var params = require('../params.js');
+var request = require('request');
+var https = require('https');
+
+/*
+    @TODO:: No Authentication required for now, will implement if needed.
+    Below url is to fetch contacts from zoho.com using organization id and
+    respected Authorization would be set in params file.
+*/
 
 //Routes
-router.get('/contacts', function (req, res) {
-    //Call API here only.
-    
-})
+router.get('/contacts', function(req, res, next) {
+    // Url to fetch the data
+    url = 'https://books.zoho.com/api/v3/organizations?organization_id='+params['organization_id']
+    let requrl = request.defaults({
+        headers: {'Authorization': ' Zoho-authtoken '+params['zohooauthtoken']}
+    });
+    requrl(url, function(err, response, body) {
+        console.log(body);
+        if (!err && response.statusCode === 200)
+        {
+            res.json(JSON.parse(body));
+        }
+        else
+        {
+            res.json(error);
+        }
+    });
+});
 
 // Return router
 module.exports = router;
